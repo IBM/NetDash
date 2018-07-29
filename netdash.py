412a3bdd42cbc32231a46f6217131c07c2756e84
Andrew Brezovsky <abrezovsky@gmail.com>
2018-07-29 00:19:57 -0400

    Added optional command line argument for ping count.
    Uses -c or -count.
    
    Signed-off-by: Andrew Brezovsky <abrezovsky@gmail.com>

--- netdash.py
+++ netdash.py
@@ -7,26 +7,28 @@ import platform
 import subprocess
 
 
-def ping(ip_addr):
-    """Ping specified IP address"""
+def ping(ip_addr, count):
+    """Ping specified IP address 'count' times"""
 
     count_parm = '-c'  # Count flag, default to unix usage
+    count_num = count  # Number of pings to send
 
     # Change count flag if on Windows system
     if platform.system() == 'Windows':
         count_parm = '-n'
 
-    return subprocess.call(['ping', count_parm, '1', ip_addr]) == 0
+    return subprocess.call(['ping', count_parm, str(count_num), ip_addr]) == 0
 
 
 addresses = []  # List of IP addresses
 
-# Remove username from log output, insert space
+# Format log, remove username and insert a space
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description="Network monitoring dashboard")
 parser.add_argument('path', help="path to configuration file")
+parser.add_argument('-c', '-count', nargs=1, type=int, default=[1], help="number of pings to send")
 args = parser.parse_args()
 
 # Open configuration file at specified path
@@ -53,4 +55,4 @@ file.close()
 
 # Ping all of the addresses
 for address in addresses:
-    ping(str(address))
+    ping(str(address), args.c[0])
